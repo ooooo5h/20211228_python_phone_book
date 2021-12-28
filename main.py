b8cd1a877c674399202c3ff5495387b18a6db320
ooooo5h <ooooo5h91@gmail.com>
2021-12-28 15:47:29 +0900

    첫 주석 추가

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from phone_book import print_menu, add_phone_num, show_all_phone_num
 from time import sleep
 
 while True:
+    # 전화번호 기능의 module에서 불러낸 함수를 사용
+    # 메뉴 출력 기능
     num = print_menu()
 
     if num == 0:

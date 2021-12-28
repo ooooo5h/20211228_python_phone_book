62cb9dcbc82cff72e9f1d4a62d602bc9ae90fc81
ooooo5h <ooooo5h91@gmail.com>
2021-12-28 15:08:54 +0900

    파일 출력 - 입력받은 전화번호부 데이터를 파일에 추가 저장
    입력아니고 출력!

--- phone_book.py
+++ phone_book.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 # 실질적인 동작 관련 코드들을 함수로 갖고있는 파일(모듈)
 
 
@@ -20,3 +22,15 @@ def add_phone_num():
     name = input('이름 : ')
     phone_num = input('전화번호 : ')
     memo = input('특이사항 : ')
+    
+    # 전화번호부 파일에 한 줄로 추가.   이름,폰번,메모 양식으로 추가해보자
+    # with 구문 > 파일 열고 닫기 자동
+    with open('phone_book.csv', 'a') as file:
+        # .csv : 이름,폰번,메모처럼 , 를 이용해서 칸 구분 => 엑셀에서 표 형태로 표현이 가능하다
+        
+        input_line = f'{name}, {phone_num}, {memo}\n'   # 3개의 데이터를 한 줄로 ,로 붙여줌 + 다음줄로 내려주기
+        file.write(input_line)
+        
+    # 안내 문구
+    print('전화번호 등록이 완료되었습니다.')   # 2초 가량 대기 후 메뉴로
+    sleep(2)
